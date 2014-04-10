c625f3f5f5dbe63a9a6b251cdcd795d5bea12109
liz rush <liz@lizmrush.com>
2014-04-10 11:11:12 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'httparty'
+gem 'sqlite3'
+
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "guard-rspec"
+  gem "factory_girl_rails"
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'simplecov', :require => false
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seapark::Application.config.secret_key_base = '93388904096f08dd3cfd35e6159cd794fae054949769684c610205956db97cb10989bd50ee6fdcb4f32adfb97cb8cc2b7ec59a70f2d00f13e76e6b43b9f54b5a'

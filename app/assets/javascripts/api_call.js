7830e14921636c2037a191dcd1f7115d974d3c49
liz rush <liz@lizmrush.com>
2014-04-15 13:06:16 -0700

    adds laoding overlay to api call

--- app/assets/javascripts/api_call.js
+++ app/assets/javascripts/api_call.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#find-parking").click(function() {
 
     map.setOptions(window.disabled_map);
+    document.getElementById('loading').style.visibility = "visible";
 
     var bbox = map.getBounds().toString();
     var center = mapcenter;
@@ -15,12 +16,13 @@ $(document).ready(function() {
       success: function(data, textStatus, xhr) {
         url = data.url
         window.set_overlay(url)
-      map.setOptions(window.enabled_map);
-
+        map.setOptions(window.enabled_map);
+        document.getElementById('loading').style.visibility = "hidden";
       },
 
       error: function(xhr, textStatus, errorThrown) {
         alert("We're sorry, something when wrong! Please try again.");
+        document.getElementById('loading').style.visibility = "hidden";
       }
     });
 

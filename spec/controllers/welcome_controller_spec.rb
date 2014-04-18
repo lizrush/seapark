107ddc099073d2d810cd1ce9c294f6b50c91f227
liz rush <liz@lizmrush.com>
2014-04-18 15:12:22 -0700

    adds test, 100% coverage

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,23 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe WelcomeController do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET #about" do
+    it "responds successfully with an HTTP 200" do
+      get :about
+      response.should render_template(:partial => '_about')
+    end
+  end
 end
